53f2641afc2e387ba94bb10a36978e42a911ac12
hamdi <javs_shao@163.com>
2019-01-04 09:50:05 +0800

    编写Spiders

--- douyu/douyu/spiders/douyuapp.py
+++ douyu/douyu/spiders/douyuapp.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
+import json
+
 import scrapy
+
+from douyu.items import DouyuItem
 
 
 class DouyuappSpider(scrapy.Spider):
     name = 'douyuapp'
     allowed_domains = ['www.douyu.com']
-    start_urls = ['http://www.douyu.com/']
+    offset = 0
+    url = "http://capi.douyucdn.cn/api/v1/getVerticalRoom?limit=20&offset="
+    start_urls = [url + str(offset)]
 
     def parse(self, response):
-        pass
+        # 返回从json里获取data段数据集合
+        data = json.loads(response.text)['data']
+        for each in data:
+            item = DouyuItem()
+            item['name'] = each['nichname']
+            item['images_urls'] = each['vertical_src']
+
+            yield item
+
+        self.offset += 20
+        yield scrapy.Request(self.url + str(self.offset), callback=self.parse)
