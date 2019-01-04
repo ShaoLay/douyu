ccc4f8f78fe09e9707dc01995605d5c7a923f199
hamdi <javs_shao@163.com>
2019-01-04 10:35:37 +0800

    Spiders_修正

--- douyu/douyu/spiders/douyuapp.py
+++ douyu/douyu/spiders/douyuapp.py
@@ -1,27 +1,25 @@
-# -*- coding: utf-8 -*-
-import json
-
 import scrapy
+import json
+from douyu.items import DouyuspiderItem
 
-from douyu.items import DouyuItem
+class DouyuSpider(scrapy.Spider):
+    name = "douyu"
+    allowd_domains = ["http://capi.douyucdn.cn"]
 
-
-class DouyuappSpider(scrapy.Spider):
-    name = 'douyuapp'
-    allowed_domains = ['www.douyu.com']
     offset = 0
     url = "http://capi.douyucdn.cn/api/v1/getVerticalRoom?limit=20&offset="
     start_urls = [url + str(offset)]
 
     def parse(self, response):
-        # 返回从json里获取data段数据集合
-        data = json.loads(response.text)['data']
-        for each in data:
-            item = DouyuItem()
-            item['name'] = each['nichname']
-            item['images_urls'] = each['vertical_src']
+      # 返回从json里获取 data段数据集合
+      data = json.loads(response.text)["data"]
 
-            yield item
+      for each in data:
+          item = DouyuspiderItem()
+          item["name"] = each["nickname"]
+          item["imagesUrls"] = each["vertical_src"]
 
-        self.offset += 20
-        yield scrapy.Request(self.url + str(self.offset), callback=self.parse)
+          yield item
+
+      self.offset += 20
+      yield scrapy.Request(self.url + str(self.offset), callback = self.parse)
