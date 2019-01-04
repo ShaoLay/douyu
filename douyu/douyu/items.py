84e078bb39aab385d483c181a3505ebf2219d7fb
hamdi <javs_shao@163.com>
2019-01-04 10:33:18 +0800

    items_修改

--- douyu/douyu/items.py
+++ douyu/douyu/items.py
@@ -4,13 +4,10 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class DouyuItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    name = scrapy.Field()
-    images_urls = scrapy.Field()
-    images_path = scrapy.Field()
+class DouyuspiderItem(scrapy.Item):
+    name = scrapy.Field()# 存储照片的名字
+    imagesUrls = scrapy.Field()# 照片的url路径
+    imagesPath = scrapy.Field()# 照片保存在本地的路径
